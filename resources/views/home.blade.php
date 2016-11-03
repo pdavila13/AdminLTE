74e511369a49ae13302b02e1e153bec9b0aa1c61
Paolo Dávila <pdavila@iesebre.com>
2016-11-03 16:30:36 +0100

    Implemented Blade template in AdminLTE project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('main-content')
+    <div class="container-fluid spark-screen">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Home
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
